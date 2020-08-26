90234e18f3e17567ef268504bf1b1f8a654e7dfd
Viviane Silva Florido <vivianeflowt@gmail.com>
2020-08-26 04:11:18 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,20 +57,6 @@ sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
 sudo apt -f install
 sleep 10
 
-# Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
-apt-cache policy docker-ce
-sleep 10
-sudo apt install -y docker-ce
-sudo usermod -aG docker ${USER}
-su - ${USER}
-id -nG
-sudo usermod -aG docker ${USER}
-docker --version
-sleep 10
-
 # zoom
 wget https://zoom.us/client/latest/zoom_amd64.deb -O zoom.deb
 sudo dpkg -i zoom.deb
