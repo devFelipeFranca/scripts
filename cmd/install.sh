964a15f5550e76aa2ce3fe8945b06e1187deea42
Viviane Silva Florido <vivianeflowt@gmail.com>
2020-08-27 12:00:00 -0300

    Create install.sh

---
+++ cmd/install.sh
@@ -0,0 +1,9 @@
+sudo chmod +x cmd.backup 
+sudo chmod +x cmd.hotkeys
+sudo chmod +x cmd.killport
+sudo chmod +x cmd.node.newapp
+
+sudo cp cmd.backup /usr/local/bin/
+sudo cp cmd.hotkeys /usr/local/bin/
+sudo cp cmd.killport /usr/local/bin/
+sudo cp cmd.node.newapp /usr/local/bin/
