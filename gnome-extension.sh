32d0b0a4f1f4394a00ebb5fc8728c7266c4e4638
Viviane Silva Florido <vivianeflowt@gmail.com>
2020-08-27 13:07:17 -0300

    Create gnome-extension.sh

---
+++ gnome-extension.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo clear
+
+xdg-open https://extensions.gnome.org/extension/15/alternatetab/
+xdg-open https://extensions.gnome.org/extension/2159/application-view-columns/
+xdg-open https://extensions.gnome.org/extension/6/applications-menu/
+xdg-open https://extensions.gnome.org/extension/16/auto-move-windows/
+xdg-open https://extensions.gnome.org/extension/779/clipboard-indicator/
+xdg-open https://extensions.gnome.org/extension/3396/color-picker/
+xdg-open https://extensions.gnome.org/extension/1465/desktop-icons/
+xdg-open https://extensions.gnome.org/extension/2224/easy-docker-containers/
+xdg-open https://extensions.gnome.org/extension/28/gtile/
+xdg-open https://extensions.gnome.org/extension/7/removable-drive-menu/
+xdg-open https://extensions.gnome.org/extension/800/remove-dropdown-arrows/
+xdg-open https://extensions.gnome.org/extension/1128/hide-activities-button/
+xdg-open https://extensions.gnome.org/extension/1112/screenshot-tool/
+xdg-open https://extensions.gnome.org/extension/600/launch-new-instance/
+xdg-open https://extensions.gnome.org/extension/18/native-window-placement/
+xdg-open https://extensions.gnome.org/extension/881/screenshot-window-sizer/
+xdg-open https://extensions.gnome.org/extension/1677/public-ip/
+xdg-open https://extensions.gnome.org/extension/1762/lan-ip-address/
+xdg-open https://extensions.gnome.org/extension/657/shelltile/
+xdg-open https://extensions.gnome.org/extension/906/sound-output-device-chooser/
+xdg-open https://extensions.gnome.org/extension/1301/ubuntu-appindicators/
+xdg-open https://extensions.gnome.org/extension/602/window-list/
+xdg-open https://extensions.gnome.org/extension/10/windownavigator/
+xdg-open https://extensions.gnome.org/extension/1485/workspace-matrix/
