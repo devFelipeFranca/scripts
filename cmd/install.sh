47a6cb74e8a8803d05ca4afb7662367542dec1a2
Viviane Silva Florido <vivianeflowt@gmail.com>
2020-08-27 12:06:54 -0300

    Update install.sh

--- cmd/install.sh
+++ cmd/install.sh
@@ -6,8 +6,10 @@ sudo chmod +x cmd.backup
 sudo chmod +x cmd.hotkeys
 sudo chmod +x cmd.killport
 sudo chmod +x cmd.node.newapp
+sudo chmod +x cmd.updateall
 
 sudo cp cmd.backup /usr/local/bin/
 sudo cp cmd.hotkeys /usr/local/bin/
 sudo cp cmd.killport /usr/local/bin/
 sudo cp cmd.node.newapp /usr/local/bin/
+sudo cp cmd.updateall /usr/local/bin/
